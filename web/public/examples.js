77c0edd6060e46b53dc274690e31c5c346c0186c
Romelianism <Romelianism@gmail.com>
2025-02-17 23:28:38 +0800

    feat(web): add compressed and uncompressed sizes

--- web/public/examples.js
+++ web/public/examples.js
@@ -1,47 +1,44 @@
-async function getFileSize(url, element) {
+async function getUncompressedSize(url, element) {
+    const fullResponse = await fetch(url);
+    if (!fullResponse.ok) return -1;
+    const blob = await fullResponse.blob();
+    const uncompressedSizeInKB = (blob.size / 1024).toFixed(1);
+    element.textContent += ` | ${uncompressedSizeInKB} KB (uncompressed)`;
+}
+
+async function getFileSize(url, element, already_uncompressed = false) {
     try {
         const response = await fetch(url, { method: 'HEAD' });
-        if (response.ok) {
-            const contentLength = response.headers.get('Content-Length');
-            if (contentLength) {
-                const sizeInKB = (parseInt(contentLength) / 1024).toFixed(1); // Convert to KB, one decimal place
-                element.textContent = `${sizeInKB} KB`;
-            } else {
-                element.textContent = 'Size N/A';
-            }
-        } else {
+        if (!response.ok) {
             element.textContent = 'Error';
+            return;
         }
-    } catch (error) {
-        console.error('Error fetching file size:', error);
+
+        const contentLength = response.headers.get('Content-Length');
+        if (contentLength) {
+            const sizeInKB = (parseInt(contentLength) / 1024).toFixed(1);
+            element.textContent = already_uncompressed ? `${sizeInKB} KB` : `${sizeInKB} KB (compressed)`;
+        }
+
+        if (!already_uncompressed) getUncompressedSize(url, element);
+    } catch {
         element.textContent = 'Error';
     }
 }
 
-// Select all example groups
-const exampleGroups = document.querySelectorAll('.example-group');
-
-exampleGroups.forEach(async group => {
-    // Within each group, find the GIF and SVG elements
+document.querySelectorAll('.example-group').forEach(async group => {
     const gifExample = group.querySelector('.example:first-child');
     const svgExample = group.querySelector('.example:last-child');
 
-    // Get the image URL and the span element for the GIF
     if (gifExample) {
         const gifImg = gifExample.querySelector('img');
         const gifSizeSpan = gifExample.querySelector('p > span');
-        if (gifImg && gifSizeSpan) {
-            getFileSize(gifImg.src, gifSizeSpan);
-        }
+        if (gifImg && gifSizeSpan) getFileSize(gifImg.src, gifSizeSpan, true);
     }
 
-
-    // Get the image URL and the span element for the SVG
     if (svgExample) {
         const svgImg = svgExample.querySelector('img');
         const svgSizeSpan = svgExample.querySelector('p > span');
-        if (svgImg && svgSizeSpan) {
-            getFileSize(svgImg.src, svgSizeSpan);
-        }
+        if (svgImg && svgSizeSpan) getFileSize(svgImg.src, svgSizeSpan, false);
     }
 });
